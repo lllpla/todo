026ff0629802dc0cd8770af57c24eb37f7d99a74
lifei <lllpla@163.com>
2020-03-22 12:32:35 +0800

    使用localstorage存储

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,14 +4,16 @@
       <v-col lg="8" sm="12" md="12" xs="12">
         <v-card id="addCard">
           <Task :taskTypes="taskTypes" :isNew="true"></Task>
-          <v-item v-for="(todo, i) in todoList" :key="i">
-            <Task
-              :taskTypes="taskTypes"
-              :task="todo"
-              :outlined="true"
-              :solo="false"
-            ></Task>
-          </v-item>
+          <v-item-group>
+            <v-item v-for="todo in todoList" :key="todo.id">
+              <Task
+                :taskTypes="taskTypes"
+                :task="todo"
+                :outlined="true"
+                :solo="false"
+              ></Task>
+            </v-item>
+          </v-item-group>
         </v-card>
       </v-col>
     </v-row>
@@ -24,13 +26,20 @@ export default {
   name: "Main",
   components: { Task: Task },
   data: () => ({
-    taskTypes: ["工作", "学习", "生活"]
+    taskTypes: ["工作", "学习", "生活"],
+    todoList: []
   }),
-  methods: {},
-  computed: {
-    todoList() {
-      return this.$store.state.todoList;
+  methods: {
+    syncTodoList() {
+      this.todoList = JSON.parse(localStorage.todoList);
     }
+  },
+  mounted() {
+    this.syncTodoList();
+    const _this = this;
+    this.$bus.$on("resetTodoList", function() {
+      _this.syncTodoList();
+    });
   }
 };
 </script>
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-form style="margin-left:10px;margin-right:10px">
+  <v-form ref="form" style="margin-left:10px;margin-right:10px">
     <v-row>
-      <v-col lg="2" md="4" sm="4" xs="4">
+      <v-col lg="2" md="4" sm="4">
         <v-select
           :items="taskTypes"
           v-model="type"
@@ -9,37 +9,68 @@
           :solo="solo"
           dense
           :outlined="outlined"
-          :disabled="disabled"
+          :disabled="thisDisabled"
         ></v-select>
       </v-col>
-      <v-col lg="10" md="8" sm="8" xs="8">
+      <v-col lg="8" md="6" sm="6">
         <v-text-field
           placeholder="请输入任务内容"
           :solo="solo"
           label="任务内容"
           dense
-          :disabled="disabled"
+          :disabled="thisDisabled"
           :outlined="outlined"
           v-model="text"
         >
           <v-btn slot="append" v-if="isNew == true" @click="addTask" icon>
             <v-icon>add_circle</v-icon>
           </v-btn>
-          <v-btn slot="append" v-if="isNew == false" @click="finishTask" icon>
-            <v-icon>done</v-icon>
-          </v-btn>
         </v-text-field>
       </v-col>
+      <v-col lg="2" md="2" sm="2">
+        <v-btn
+          v-if="isNew == false && state == 'start'"
+          @click="finishTask"
+          icon
+          color="green"
+        >
+          <v-icon>done</v-icon>
+        </v-btn>
+        <v-btn
+          v-if="state == 'finish'"
+          color="primary"
+          @click="restoreTask"
+          icon
+        >
+          <v-icon>restore</v-icon>
+        </v-btn>
+        <v-btn v-if="state == 'finish'" color="error" @click="deleteTask" icon>
+          <v-icon>delete</v-icon>
+        </v-btn>
+      </v-col>
     </v-row>
+    <p class="text-right" style="font-size:smaller" v-if="date != ''">
+      {{ date }}
+    </p>
+    <v-divider></v-divider>
+    <v-snackbar v-model="snackbar" :timeout="1000" top color="error">
+      任务不能为空
+    </v-snackbar>
   </v-form>
 </template>
 <script>
+import uuidv1 from "uuid/v1";
 export default {
   name: "Task",
   data() {
     return {
+      snackbar: false,
+      id: "",
       type: "",
-      text: ""
+      text: "",
+      state: "",
+      date: "",
+      thisDisabled: false
     };
   },
   props: {
@@ -50,7 +81,7 @@ export default {
     task: {
       type: Object,
       default: function() {
-        return { type: "工作", text: "" };
+        return { type: "工作", text: "", state: "" };
       }
     },
     taskTypes: Array,
@@ -69,19 +100,67 @@ export default {
   },
   methods: {
     addTask() {
+      if (this.text === "") {
+        this.snackbar = true;
+        return;
+      }
       const oneTask = {
+        id: uuidv1(),
         type: this.type,
-        text: this.text
+        text: this.text,
+        date: new Date().toLocaleString(),
+        state: "start"
       };
-      this.$store.commit("add", oneTask);
+      let list = this.getTodoList();
+      list.unshift(oneTask);
+      this.setTodoList(list);
+      this.type == "工作";
+      this.text = "";
     },
     finishTask() {
-      this.disabled = true;
+      this.thisDisabled = true;
+      let list = this.getTodoList();
+      let task = list.filter(todo => {
+        return todo.id == this.id;
+      })[0];
+      task.state = "finish";
+      this.setTodoList(list);
+      this.state = "finish";
+    },
+    restoreTask() {
+      this.thisDisabled = false;
+      let list = this.getTodoList();
+      let task = list.filter(todo => {
+        return todo.id == this.id;
+      })[0];
+      task.state = "start";
+      this.setTodoList(list);
+      this.state = "start";
+    },
+    deleteTask() {
+      let list = this.getTodoList();
+      list = list.filter(todo => todo.id != this.id);
+      this.setTodoList(list);
+    },
+    getTodoList() {
+      if (localStorage.todoList) {
+        return JSON.parse(localStorage.todoList);
+      } else {
+        return [];
+      }
+    },
+    setTodoList(todoList) {
+      localStorage.todoList = JSON.stringify(todoList);
+      this.$bus.$emit("resetTodoList");
     }
   },
   mounted() {
+    this.id = this.task.id;
     this.type = this.task.type;
     this.text = this.task.text;
+    this.state = this.task.state;
+    this.thisDisabled = this.disabled;
+    this.date = this.task.date;
   }
 };
 </script>
