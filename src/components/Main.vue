c85f62ed1c5ee5b551dcf57532602fddfb678a93
lifei <lllpla@163.com>
2020-03-23 10:20:29 +0800

    使用localstorage存储

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,6 +3,51 @@
     <v-row align="center" justify="center">
       <v-col lg="8" sm="12" md="12" xs="12">
         <v-card>
+          <v-tabs grow background-color="white" color="deep-purple accent-4">
+            <v-tab>
+              <v-icon left>access_alarm</v-icon>
+              进行中
+            </v-tab>
+            <v-tab>
+              <v-icon left>style</v-icon>
+              已完成
+            </v-tab>
+            <v-tab-item>
+              <v-card
+                flat
+                height="80vh"
+                style="overflow-y:auto;overflow-x:hidden;"
+              >
+                <v-item-group>
+                  <v-item v-for="todo in getTodoList('start')" :key="todo.id">
+                    <Task
+                      :taskTypes="taskTypes"
+                      :task="todo"
+                      :outlined="true"
+                      :solo="false"
+                    ></Task>
+                  </v-item>
+                </v-item-group>
+              </v-card>
+            </v-tab-item>
+            <v-tab-item>
+              <v-card
+                flat
+                height="80vh"
+                style="overflow-y:auto;overflow-x:hidden;"
+                ><v-item-group>
+                  <v-item v-for="todo in getTodoList('finish')" :key="todo.id">
+                    <Task
+                      :taskTypes="taskTypes"
+                      :task="todo"
+                      :outlined="true"
+                      :solo="false"
+                    ></Task>
+                  </v-item>
+                </v-item-group>
+              </v-card>
+            </v-tab-item>
+          </v-tabs>
           <v-item-group>
             <v-item v-for="todo in getTodoList()" :key="todo.id">
               <Task
@@ -16,25 +61,6 @@
         </v-card>
       </v-col>
     </v-row>
-    <v-footer class="text-center" app height="50">
-      <v-chip-group
-        v-model="amenities"
-        style="margin-left:10px"
-        active-class="primary--text"
-        column
-        multiple
-      >
-        <v-chip
-          small
-          filter
-          outlined
-          v-for="chip in chips"
-          :key="chip"
-          :value="chip"
-          >{{ chip }}</v-chip
-        >
-      </v-chip-group>
-    </v-footer>
   </v-container>
 </template>
 
@@ -46,28 +72,14 @@ export default {
   components: { Task: Task },
   data: () => ({
     taskTypes: ["工作", "学习", "生活"],
-    todoList: [],
-    amenities: ["工作", "学习", "生活", "已完成", "进行中"],
-    chips: ["工作", "学习", "生活", "进行中", "已完成"]
+    todoList: []
   }),
   methods: {
     syncTodoList() {
       this.todoList = JSON.parse(localStorage.todoList);
     },
-    getTodoList() {
-      let list = this.todoList;
-      if (this.amenities.indexOf("已完成") === -1) {
-        list = list.filter(todo => todo.state !== "finish");
-      }
-      if (this.amenities.indexOf("进行中") === -1) {
-        list = list.filter(todo => todo.state !== "start");
-      }
-      this.taskTypes.forEach(oneType => {
-        if (this.amenities.indexOf(oneType) === -1) {
-          list = list.filter(todo => todo.type !== oneType);
-        }
-      });
-
+    getTodoList(state) {
+      let list = this.todoList.filter(todo => todo.state === state);
       return list;
     }
   },
