ae696118510e3d48d104e7449b61698fb3c5bf81
lifei <lllpla@163.com>
2020-04-25 20:30:04 +0800

    '修复获取token为空时页面空白问题'

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -6,16 +6,16 @@
       </q-card-section>
       <q-separator />
       <q-card-section>
-        <q-input outlined v-model="settings.token" label="token" />
+        <q-input outlined v-model="token" label="token" />
       </q-card-section>
       <q-card-section>
-        <q-input outlined v-model="settings.apiUrl" label="apiUrl" />
+        <q-input outlined v-model="apiUrl" label="apiUrl" />
       </q-card-section>
       <q-card-section>
-        <q-input outlined v-model="settings.user" label="user" />
+        <q-input outlined v-model="user" label="user" />
       </q-card-section>
       <q-card-section>
-        <q-input outlined v-model="settings.repo" label="repo" />
+        <q-input outlined v-model="repo" label="repo" />
       </q-card-section>
       <q-card-section>
         <q-btn class="full-width" @click="saveSettings" color="primary"
@@ -29,6 +29,14 @@
 import { mapState } from "vuex";
 export default {
   name: "now",
+  data() {
+    return {
+      apiUrl: "",
+      token: "",
+      user: "",
+      repo: ""
+    };
+  },
   computed: {
     ...mapState({
       settings: state => {
@@ -36,16 +44,23 @@ export default {
       }
     })
   },
+  mounted() {
+    this.apiUrl = this.settings.apiUrl;
+    this.token = this.settings.token;
+    this.user = this.settings.user;
+    this.repo = this.settings.repo;
+  },
   methods: {
     saveSettings() {
       const settings = {
-        token: settings.token,
-        apiUrl: settings.apiUrl,
-        user: settings.user,
-        repo: settings.repo
+        token: this.token,
+        apiUrl: this.apiUrl,
+        user: this.user,
+        repo: this.repo
       };
       this.$q.localStorage.set("lee-todo-settings", settings);
       this.$store.commit("initSettings", { settings: settings });
+      this.$store.dispatch("init");
       this.$router.push({
         path: "/now"
       });
